87b16cb1d22b181903a7ee67fd833bc57099de22
Jonathan Wesley Stone <js@piratejon.com>
2014-03-25 23:04:34 -0500

    looking gravy

--- update.sh
+++ update.sh
@@ -1,29 +1,58 @@
 #!/bin/sh
 
-sudo pacman -Sy
+update_repositories () {
+  sudo pacman -Sy
+}
 
-PKGLIST=packages-`date +%F`
-pacman -Qu | awk '{print $1}' > ${PKGLIST}
-
-for pkg in `cat ${PKGLIST}`
-do
-  for file in `pacman -Ql ${pkg} | cut --complement -d' ' -f1`
+date_based_folder () {
+  _today=`date +%F`
+  _try=${_today}
+  _i=0
+  while [ -e ${_try} ]
   do
-    [ -f "${file}" ] && {
-      md5sum "${file}" >> "${pkg}-presums"
-    }
+    _try="${_today}${_i}"
   done
-done
 
-echo y | sudo pacman -Su
+  mkdir "${_try}" && {
+    OUTDIR="${_try}"
+  } || {
+    echo "Error: couldn't make ${_try}" 1>&2
+    exit 1
+  }
+}
 
-for pkg in `cat ${PKGLIST}`
-do
-  for file in `pacman -Ql ${pkg} | cut --complement -d' ' -f1`
+date_based_folder
+
+echo "Outputting to ${OUTDIR}"
+
+PKGLIST="${OUTDIR}/packages"
+STDOUT="${OUTDIR}/stdout"
+STDERR="${OUTDIR}/stderr"
+
+save_package_list () {
+  pacman -Qu | awk '{print $1}' > ${PKGLIST}
+}
+
+make_great_hashes () {
+  _suffix="${1}"
+  for _pkg in `cat ${PKGLIST}`
   do
-    [ -f "${file}" ] && {
-      md5sum "${file}" >> "${pkg}-postsums"
-    }
+    for _file in `pacman -Ql ${_pkg} | cut --complement -d' ' -f1`
+    do
+      [ -f "${_file}" ] && {
+        sudo sha256sum "${_file}"
+      }
+    done > "${OUTDIR}/${_pkg}-${_suffix}"
   done
-done
+}
+
+do_a_real_update () {
+  echo y | sudo pacman -Su 2>${STDERR} | tee ${STDOUT}
+}
+
+update_repositories
+save_package_list
+make_great_hashes presums
+do_a_real_update
+make_great_hashes postsums
 
