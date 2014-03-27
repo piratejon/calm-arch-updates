19695a309def68348160d2f8809498915c305a7b
Jonathan Wesley Stone <js@piratejon.com>
2014-03-26 21:40:27 -0500

    save the starting package version

--- update.sh
+++ update.sh
@@ -21,21 +21,13 @@ date_based_folder () {
   }
 }
 
-date_based_folder
-
-echo "Outputting to ${OUTDIR}"
-
-PKGLIST="${OUTDIR}/packages"
-STDOUT="${OUTDIR}/stdout"
-STDERR="${OUTDIR}/stderr"
-
 save_package_list () {
-  pacman -Qu | awk '{print $1}' > ${PKGLIST}
+  pacman -Qu > ${PKGLIST}
 }
 
 make_great_hashes () {
   _suffix="${1}"
-  for _pkg in `cat ${PKGLIST}`
+  for _pkg in `cat ${PKGLIST} | awk '{print $1}'`
   do
     for _file in `pacman -Ql ${_pkg} | cut --complement -d' ' -f1`
     do
@@ -49,6 +41,17 @@ make_great_hashes () {
 do_a_real_update () {
   echo y | sudo pacman -Su 2>${STDERR} | tee ${STDOUT}
 }
+
+set_globals () {
+  PKGLIST="${OUTDIR}/packages"
+  STDOUT="${OUTDIR}/stdout"
+  STDERR="${OUTDIR}/stderr"
+}
+
+date_based_folder
+set_globals
+
+echo "Outputting to ${OUTDIR}"
 
 update_repositories
 save_package_list
